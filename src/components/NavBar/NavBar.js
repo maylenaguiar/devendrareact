048167e195c8c40d02f9ca77672eadb07a38a7f8
maylen aguiar <maylenaguiar.psi@gmail.com>
2022-01-31 21:22:52 -0300

    NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+const Navbar = () => {
+  return <div>
+      <nav className="container">
+      <ul>
+        <li>Inicio</li>
+        <li>Sobre mi</li>
+        <li>Sahumerios</li>
+        <li>Adornos</li>
+      </ul>
+    </nav>
+  </div>;
+};
+
+export default Navbar;
