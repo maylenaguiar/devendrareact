d89087acce27bfe588db5a149ec0786c283b603f
maylen aguiar <maylenaguiar.psi@gmail.com>
2022-01-31 22:14:41 -0300

    desafio 2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,11 +4,11 @@ import './NavBar.css';
 const Navbar = () => {
   return <div>
       <nav className="container">
-      <ul>
-        <li>Inicio</li>
-        <li>Sobre mi</li>
-        <li>Sahumerios</li>
-        <li>Adornos</li>
+      <ul className='naveg'>
+        <li className='naveg'>Devendra</li>
+        <li className='naveg'>Sobre mi</li>
+        <li className='naveg'>Sahumerios</li>
+        <li className='naveg'>Adornos</li>
       </ul>
     </nav>
   </div>;
